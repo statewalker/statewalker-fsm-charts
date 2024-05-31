593b2a486347874c6bb72398f358bb2f318d5c34
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-05-31 20:30:06 +0200

    chore: cleanup tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts", "src/index-layout.ts", "src/index-svg.ts"],
-  // entryPoints: ["src/index-svg.ts"],
   clean: true,
   dts: true,
   treeshake: true,
