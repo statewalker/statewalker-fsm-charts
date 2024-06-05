b40a3612c3e9f64718bcb270755315f835963692
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-06-05 08:31:41 +0200

    chore: add runtime api placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./index-layout.ts";
 export * from "./index-svg.ts";
+export * from "./index-runtime.ts";
 export * from "./utils/toKebabCase.ts";
 export * from "./utils/serializeCss.ts";
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts", "src/index-layout.ts", "src/index-svg.ts"],
+  entryPoints: [
+    "src/index.ts",
+    "src/index-layout.ts",
+    "src/index-svg.ts",
+    "src/index-runtime.ts",
+  ],
   clean: true,
   dts: true,
   treeshake: true,
