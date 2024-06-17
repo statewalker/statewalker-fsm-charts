83b1fb1d895ea1d31dd452683d292ffc658b3e17
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-06-17 02:05:35 +0200

    chore: add comments to types refactors

--- src/types/FsmStateConfig.ts
+++ src/types/FsmStateConfig.ts
@@ -1,5 +1,15 @@
 import type { Transition } from "./Transition.ts";
 
+// export type FsmStateKey = string;
+// export type FsmEventKey = string;
+// export type FsmTransition = [FsmStateKey, FsmEventKey, FsmStateKey];
+// export type FsmState = {
+//   key: string;
+//   transitions: FsmTransition[];
+//   states?: FsmState[];
+// } & Record<string, any>;
+
+
 export type FsmStateConfig = {
   key: string;
   transitions: Transition[];
