9e41e66f7698c9081c788b39b5eaaaf0966c4c1f
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-10-15 09:48:35 +0200

    chore: update format

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -4,46 +4,87 @@ export type DomSection = {
   children: DomSection[];
 };
 
-export function getStateDescriptionRenderer(
-  docNode: HTMLElement,
-  rootStateKey: string,
-) {
-  const docSections = buildSections(docNode);
-  const rootStateFilter = getStateDescriptionFilter(rootStateKey);
+export function getStateDescriptionRenderer({
+  element,
+  rootStateKey,
+  splitHeader = (
+    header?: HTMLElement,
+  ): [stateKey: undefined | string, label: string] => {
+    if (!header) return [undefined, ""];
+    const text = header.innerText.trim();
+    let key: string | undefined;
+    let label: string = text;
+    text.replace(/^\s*\[(.*)\]\s*(.*)$/, (_, k, l) => {
+      key = k.trim();
+      label = l.trim() || key;
+      return "";
+    });
+    return [key, label];
+  },
+}: {
+  element: HTMLElement;
+  rootStateKey: string;
+  splitHeader: (
+    header?: HTMLElement,
+  ) => [stateKey: undefined | string, label: string];
+}) {
+  const docSections = buildSections(element);
+  const rootStateFilter = getStateDescriptionFilter(splitHeader, rootStateKey);
   const rootDescription = findSection(docSections, rootStateFilter);
 
   if (!rootDescription) {
     return () => {};
   }
+
   // Remove description nodes from DOM
-  extractSectionDomNodes(rootDescription);
+  for (const node of visitSectionNodes(rootDescription)) {
+    if (node.nodeName[0] === "#") continue;
+    node.parentElement?.removeChild(node);
+  }
+  //   for (const section of vistSections(rootDescription)) {
+  //     const [key, label] = splitHeader(section.header);
+  //     console.log("??????", section.header?.innerText, key);
+  //     if (!key) continue;
+
+  //     section.header?.remove();
+  //     for (const node of section.content) {
+  //       if (node.nodeName[0] === "#") continue;
+  //       node.parentElement?.removeChild(node);
+  //     }
+  //   }
 
   return (statesStack: string[]) => {
     const stateKey = [...statesStack].pop();
-    const filter = getStateDescriptionFilter(stateKey);
+    const filter = getStateDescriptionFilter(splitHeader, stateKey);
     const description = findSection(rootDescription, filter);
-    const elm = renderSections(description, (s) => !rootStateFilter(s));
+    if (!description) return;
+    const elm = renderSections({
+      section: description,
+      deep: (s) => !rootStateFilter(s),
+      header: (header) => {
+        const [key, label] = splitHeader(header);
+        if (key === stateKey) {
+          return label || key;
+        }
+        return;
+      },
+    });
     const details = elm?.querySelector("details");
     details && (details.open = true);
     return elm;
   };
 
-  function getStateDescriptionFilter(stateKey?: string) {
+  function getStateDescriptionFilter(
+    splitHeader: (header: HTMLElement) => [stateKey?: string, label?: string],
+    stateKey?: string,
+  ) {
     if (!stateKey) return () => false;
     return (section: DomSection) => {
-      const text = section.header?.innerText.trim();
-      // if (text === stateKey) return true;
-      return !!(text?.indexOf(stateKey) === 0);
+      if (!section.header) return false;
+      const [key] = splitHeader(section.header);
+      return key === stateKey;
     };
   }
-}
-
-export function extractSectionDomNodes(section: DomSection): DomSection {
-  for (const node of visitSectionNodes(section)) {
-    if (node.nodeName[0] === "#") continue;
-    node.parentElement?.removeChild(node);
-  }
-  return section;
 }
 
 export function* visitDom(elm: Node): Iterable<Node> {
@@ -159,10 +200,15 @@ export function buildSections(root: HTMLElement | Iterable<Node>): DomSection {
     : top;
 }
 
-export function renderSections(
-  section?: DomSection,
-  deep: (s: DomSection) => boolean = () => true,
-) {
+export function renderSections({
+  section,
+  deep = (s) => true,
+  header = (headerElm) => headerElm,
+}: {
+  section: DomSection;
+  deep?: (s: DomSection) => boolean;
+  header?: (header: HTMLElement) => undefined | string | HTMLElement;
+}) {
   const container = document.createElement("div");
   const id = `id-${String(Math.random()).split(".")[1]}-${Date.now()}`;
   container.setAttribute("id", id);
@@ -205,7 +251,8 @@ export function renderSections(
     details.appendChild(summary);
     const content = document.createElement("div");
     details.appendChild(content);
-    summary.append(section.header?.innerText || " # ");
+    const headerElm = header(section.header!);
+    headerElm && summary.append(headerElm);
     for (const node of section.content) {
       if (node.nodeName[0] === "#") continue;
       content.append(node.cloneNode(true));
